d1b6ae7ea711d27c1459c091dc0c11450dc78132
Kevin Mason <krmason2020@gmail.com>
2022-11-26 22:06:58 -0800

    refinement

--- intervals/1288-remove-covered-intervals.py
+++ intervals/1288-remove-covered-intervals.py
@@ -2,14 +2,13 @@
 
 class Solution:
     def removeCoveredIntervals(self, arr):
-        arr.sort(key=lambda x: (x[0], x[1]))
+        # sort second el larger to smaller that way the interval to cover always comes first in res
+        arr.sort(key=lambda x: (x[0], -x[1]))
         res = []
         res.append(arr[0])
         for el in arr[1:len(arr)]:
             lastRes = res[len(res)-1]
-            if lastRes[0] == el[0]:
-                lastRes[1] = el[1]
-            elif not lastRes[1] >= el[1]:
+            if not lastRes[1] >= el[1]:
                 res.append(el)
         return len(res)
 
