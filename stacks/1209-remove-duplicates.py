a23d16eac424a402e56a09781ebb297f1da30d73
Kevin Mason <krmason2020@gmail.com>
2022-12-03 16:37:17 -0800

    stacks

---
+++ stacks/1209-remove-duplicates.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeDuplicates(self, str, k):
+        stack = []  # [char,count]
+
+        return "".join(stack)
+
+
+print(Solution().removeDuplicates("deeedbbcccbdaa", 3))
