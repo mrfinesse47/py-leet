5a8abe0cca3b9df1455af9e33eb63662a4807a1a
Kevin Mason <krmason2020@gmail.com>
2022-12-03 17:04:05 -0800

    stack

--- stacks/1209-remove-duplicates.py
+++ stacks/1209-remove-duplicates.py
@@ -1,8 +1,19 @@
 class Solution:
     def removeDuplicates(self, str, k):
         stack = []  # [char,count]
+        res = ""
+        for char in str:
+            if len(stack) and char == stack[-1][0]:
+                stack[-1][1] += 1
+                if stack[-1][1] == k:
+                    stack.pop()
+            else:
+                stack.append([char, 1])
 
-        return "".join(stack)
+        for el in stack:
+            res += el[0] * el[1]
+
+        return res
 
 
-print(Solution().removeDuplicates("deeedbbcccbdaa", 3))
+print(Solution().removeDuplicates("ddd", 2))
