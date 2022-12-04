1dd504bd1bfeda84bc1bcba0222e4a18a632d276
Kevin Mason <krmason2020@gmail.com>
2022-12-04 15:42:28 -0800

    more

---
+++ trees/tree.py
@@ -0,0 +1,10 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class BST:
+    def __init__(self, rootval):
+        self.root = TreeNode(rootval)
