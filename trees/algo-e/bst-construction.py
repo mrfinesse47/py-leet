24d7a2ae0682861c7e58fae84bd746c4529279fd
Kevin Mason <krmason2020@gmail.com>
2022-12-04 14:44:17 -0800

    a

---
+++ trees/algo-e/bst-construction.py
@@ -0,0 +1,23 @@
+# Do not edit the class below except for
+# the insert, contains, and remove methods.
+# Feel free to add new properties and methods
+# to the class.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        # Write your code here.
+        # Do not edit the return statement of this method.
+        return self
+
+    def contains(self, value):
+        # Write your code here.
+        pass
+
+    def remove(self, value):
+        # Write your code here.
+        # Do not edit the return statement of this method.
+        return self
--- trees/algo-e/find-closest-value.py
+++ trees/algo-e/find-closest-value.py
@@ -1,8 +1,19 @@
 def findClosestValueInBst(tree, target):
-    if target == tree.value:
-        return target
-
-    pass
+    node = tree
+    mindiff = float("inf")
+    closest = None
+    while node:
+        diff = abs(target - node.value)
+        if diff < mindiff:
+            mindiff = diff
+            closest = node.value
+        if target == node.value:
+            return node.value
+        elif target < node.value:
+            node = node.left
+        else:
+            node = node.right
+    return closest
 
 
 # This is the class of the input tree. Do not edit.
@@ -16,11 +27,11 @@ class BST:
 tree = BST(10)
 tree.right = BST(15)
 tree.right.left = BST(13)
-tree.right.left = BST(22)
+tree.right.right = BST(22)
 tree.right.left.right = BST(14)
 tree.left = BST(5)
 tree.left.right = BST(5)
 tree.left.left = BST(2)
 tree.left.left.left = BST(1)
 
-print(findClosestValueInBst(tree, 13))
+print(findClosestValueInBst(tree, 6))
