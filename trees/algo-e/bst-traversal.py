2c01779baae84058b961a5e078ef403e3f6195bc
Kevin Mason <krmason2020@gmail.com>
2022-12-07 19:23:47 -0800

    different order traversals

--- trees/algo-e/bst-traversal.py
+++ trees/algo-e/bst-traversal.py
@@ -2,36 +2,33 @@ import tree
 
 
 def inOrderTraverse(tree, array):
-    # Write your code here.
-    if tree is None:
-        return array
-    inOrderTraverse(tree.left, array)
-    array.append(tree.value)
-    inOrderTraverse(tree.right, array)
+    if tree is not None:
+        inOrderTraverse(tree.left, array)
+        array.append(tree.value)
+        inOrderTraverse(tree.right, array)
     return array
 
 
 def preOrderTraverse(tree, array):
-    if tree is None:
-        return array
-    array.append(tree.value)
-    inOrderTraverse(tree.left, array)
-    inOrderTraverse(tree.right, array)
+    if tree is not None:
+        array.append(tree.value)
+        preOrderTraverse(tree.left, array)
+        preOrderTraverse(tree.right, array)
     return array
 
 
 def postOrderTraverse(tree, array):
-    if tree is None:
-        return array
-    inOrderTraverse(tree.left, array)
-    inOrderTraverse(tree.right, array)
-    array.append(tree.value)
+    if tree is not None:
+
+        postOrderTraverse(tree.left, array)
+        postOrderTraverse(tree.right, array)
+        array.append(tree.value)
     return array
 
 
 bst = tree.BST(10).insert(5).insert(15).insert(
     2).insert(5).insert(1).insert(22)
 
-print(inOrderTraverse(bst, []))
-print(preOrderTraverse(bst, []))
+# print(inOrderTraverse(bst, []))
+# print(preOrderTraverse(bst, []))
 print(postOrderTraverse(bst, []))
