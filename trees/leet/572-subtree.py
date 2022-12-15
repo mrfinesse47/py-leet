8caf8148a57a2cc9649125d1fcf1a6376a76d3d0
Kevin Mason <krmason2020@gmail.com>
2022-12-14 19:55:41 -0800

    subtree

---
+++ trees/leet/572-subtree.py
@@ -0,0 +1,11 @@
+# 572. Subtree of Another Tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root, subRoot, origSubRoot=None):
+        pass
